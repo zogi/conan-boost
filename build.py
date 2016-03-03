cd85aefc84e5498491938fa80d2b5cb580e36e48
Luis <lasote@gmail.com>
2016-03-03 13:10:13 +0100

    PUre c false for boost

--- build.py
+++ build.py
@@ -3,6 +3,6 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds(shared_option_name="Boost:shared", pure_c=True)
+    builder.add_common_builds(shared_option_name="Boost:shared", pure_c=False)
     builder.run()
 
