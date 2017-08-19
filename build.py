7811efe3f8ab50f52a7a91180795be6b0f7eebe3
Zoltan Gilian <zoltan.gilian@gmail.com>
2017-08-19 10:28:49 +0200

    build missing dependencies

--- build.py
+++ build.py
@@ -2,6 +2,6 @@ from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="lasote", channel="stable")
+    builder = ConanMultiPackager(username="lasote", channel="stable", args="--build=missing")
     builder.add_common_builds(shared_option_name="Boost:shared")
     builder.run()
